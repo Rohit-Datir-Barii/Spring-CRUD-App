a0b98a53f20293cd0d8c9298e32a452a03d4caae
Rohit-Datir-Barii <rohitdatir143@gmail.com>
2023-04-22 12:43:31 +0530

    Removing uneccesories line

--- src/main/java/com/rd/exception/EmployeeNotFoundException.java
+++ src/main/java/com/rd/exception/EmployeeNotFoundException.java
@@ -5,9 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY) // 422
 public class EmployeeNotFoundException extends RuntimeException {
-
 	private static final long serialVersionUID = 1L;
-
 	public EmployeeNotFoundException(String msg) {
 		 super(msg);
 	}
